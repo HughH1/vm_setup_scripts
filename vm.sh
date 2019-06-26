32d4b143a7caa6ae0487aabd149a5f4a8079a570
Hugh Harding <hughfharding@gmail.com>
2019-06-26 17:17:32 +0100

    small change

--- vm.sh
+++ vm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-az vm create --resource-group MyResourceGroup --name MyVirtualMachineOne --image ubuntults --nics MyInterfaceOne --generate-ssh-keys
+az vm create --resource-group MyResourceGroup --name MyVirtualMachineOne --image ubuntults --nics MyInterfaceOne --generate-ssh-keys --admin-username admin321 --admin-password 12345Password!
 
-az vm create --resource-group MyResourceGroup --name MyVirtualMachineTwo --image ubuntults --nics MyInterfaceTwo --generate-ssh-keys
+az vm create --resource-group MyResourceGroup --name MyVirtualMachineTwo --image ubuntults --nics MyInterfaceTwo --generate-ssh-keys --admin-username admin432 --admin-password 12345Password!
  
